422d4efa9618939d2c37ed18817c2993b99d813b
Viva <happybird00@naver.com>
2025-12-03 19:54:58 +0900

    Initial commit

---
+++ src/main/java/org/example/kafkaredisspring/KafkaRedisSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.kafkaredisspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRedisSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaRedisSpringApplication.class, args);
+    }
+
+}
